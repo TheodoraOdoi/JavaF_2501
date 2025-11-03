084a681c1ff86c1697a79df7e6ead42ac0447d1f
Theodora <theodoranaana125@gmail.com>
2025-11-03 11:02:01 +0300

    add sess03 package with it's classes

---
+++ src/sess03_selection_and_iteration/LoopsFirstTenInts.java
@@ -0,0 +1,22 @@
+package sess03_selection_and_iteration;
+
+/**
+ *Java program to demonstrate the if, while, do...while loops to display
+ * the first 10 integers.
+ * 
+ * @author todoi
+ */
+public class LoopsFirstTenInts
+{
+    // Main method begins program execution
+    public static void main(String[] args)
+    {
+        // variable to be used in the program
+        int n;
+        System.out.println("First ten integers using a for loop");
+        for(n = 1; n <= 10; n++)
+            System.out.printf("%02d\n",n);
+        
+        
+    }
+}
